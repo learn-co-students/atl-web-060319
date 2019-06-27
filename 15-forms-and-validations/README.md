688f1870c204f1db6d2f22f7fa6d94d62fce35fe
kingcons <brit@kingcons.io>
2019-06-27 09:43:06 -0400

    Add forms and validations starter code.

---
+++ 15-forms-and-validations/README.md
@@ -0,0 +1,30 @@
+## Today
+
+Good Morning! http://i.imgur.com/Jdt1rRf.gifv
+
+### To Build...
+
+A tiny library.
+
+<!-- * make a wonderful homepage -->
+<!-- * list all the books -->
+<!-- * get info on a single book -->
+<!-- * add a book to the library -->
+<!-- * have a page for listing authors in the library -->
+<!-- * have a form for adding an author and several of their books at once! -->
+<!-- * Separate authors and books -->
+   * Add author model and migrate books to have an author_id
+   * Add a form for creating an author and several books at the same time
+
+## Waking Up
+
+* Break our routes into separate controllers to keep the code organized!
+
+* Let's fix the add book form to let us pick from the existing authors with a dropdown.
+
+## Editing & Validations
+
+* It would be nice to be able to edit a book. Let's add this so at least one controller supports the full set of RESTful routes.
+
+* Now add some validations. Does saving always work?
+  * How can we improve the user experience if a record doesn't save?
---
+++ 15-forms-and-validations/app/controllers/application_controller.rb
@@ -0,0 +1,60 @@
+class ApplicationController < Sinatra::Base
+  set :views, 'app/views'
+  set :method_override, true
+
+  def initialize
+    puts "\n\n>>>>> Building a new AppController: #{self} \n\n"
+    super
+  end
+
+  get '/' do
+    erb :homepage
+  end
+
+  get '/books' do
+    @books = Book.all
+    erb :"books/index"
+  end
+
+  get '/books/new' do
+    erb :"books/new"
+  end
+
+  get '/books/:id' do
+    @book = Book.find(params[:id])
+    erb :"books/show"
+  end
+
+  post '/books' do
+    @book = Book.create(params)
+    redirect "/books/#{@book.id}"
+  end
+
+  get '/authors' do
+    @authors = Author.all
+    erb :"authors/index"
+  end
+
+  get '/authors/new' do
+    erb :"authors/new"
+  end
+
+  post '/authors' do
+    @author = Author.create(full_name: params[:full_name])
+    params[:books].each do |book_info|
+      Book.create(
+        author_id: @author.id,
+        title: book_info[:title],
+        snippet: book_info[:snippet]
+      )
+    end
+    redirect "/authors"
+  end
+
+  delete '/books/:id' do
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect "/books"
+  end
+
+end
