62cc7efd81100c7450953f2589165f1966f88604
Safae <safae.alaoui@flatironschool.com>
2019-07-19 12:22:52 -0400

    api

---
+++ 25-rails-api/book-api/db/seeds.rb
@@ -0,0 +1,98 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Book.create([
+    {
+      
+        title: "Children of Blood & Bone",
+        author: "Tomi Adeyemi",
+        img: "https://images-na.ssl-images-amazon.com/images/I/61SB-c0mHGL._SX329_BO1,204,203,200_.jpg"
+      },
+      {
+        
+        author: "Zadie Smith",
+        img: "https://images-na.ssl-images-amazon.com/images/I/41oS2tKs2TL._SX324_BO1,204,203,200_.jpg",
+        title: "On Beauty"
+      },
+      {
+       
+        author: "Zadie Smith",
+        img: "https://images-na.ssl-images-amazon.com/images/I/51Vfwe7%2BJ5L._SX324_BO1,204,203,200_.jpg",
+        title: "Swing Time"
+      },
+      {
+       
+        author: "Zadie Smith",
+        img: "https://images-na.ssl-images-amazon.com/images/I/517BqHtwIKL._SX324_BO1,204,203,200_.jpg",
+        title: "Feel Free"
+      },
+      {
+       
+        author: "Maya Angelou",
+        img: "https://images-na.ssl-images-amazon.com/images/I/41SKsBaGXRL._SX301_BO1,204,203,200_.jpg",
+        title: "I Know Why the Caged Bird Sings"
+      },
+      {
+       
+        author: "Toni Morrison ",
+        img: "https://images-na.ssl-images-amazon.com/images/I/413-xT0izBL._SX322_BO1,204,203,200_.jpg",
+        title: "The Bluest Eye"
+      },
+      {
+        title: "War And Peace",
+        author: "Leo Tolstoy",
+        img: "https://images-na.ssl-images-amazon.com/images/I/51J1nb00FLL._SX330_BO1,204,203,200_.jpg"
+       
+      },
+      {
+        title: "War And Peace",
+        author: "Leo Tolstoy",
+        img: "https://images-na.ssl-images-amazon.com/images/I/51J1nb00FLL._SX330_BO1,204,203,200_.jpg"
+       
+      },
+      {
+        title: "War And Peace",
+        author: "Leo Tolstoy",
+        img: "https://images-na.ssl-images-amazon.com/images/I/51J1nb00FLL._SX330_BO1,204,203,200_.jpg",
+      
+      },
+      {
+        title: "War And Peace",
+        author: "Leo Tolstoy",
+        img: "https://images-na.ssl-images-amazon.com/images/I/51J1nb00FLL._SX330_BO1,204,203,200_.jpg"
+      
+      },
+      {
+        title: "Good Night Moon",
+        author: "Margaret Weiss Brown",
+        img: "https://images-na.ssl-images-amazon.com/images/I/61ZfoLVFjnL._SX258_BO1,204,203,200_.jpg",
+      
+      },
+      {
+        title: "Good Night Moon",
+        author: "Margaret Weiss Brown",
+        img: "https://images-na.ssl-images-amazon.com/images/I/61ZfoLVFjnL._SX258_BO1,204,203,200_.jpg"
+      
+      },
+   
+      {
+        title: "Good Night Moon",
+        author: "Margaret Weiss Brown",
+        img: "https://images-na.ssl-images-amazon.com/images/I/61ZfoLVFjnL._SX258_BO1,204,203,200_.jpg"
+    
+      },
+      {
+        title: "Good Night Moon",
+        author: "Margaret Weiss Brown",
+        img: "https://images-na.ssl-images-amazon.com/images/I/61ZfoLVFjnL._SX258_BO1,204,203,200_.jpg"
+     
+      }
+
+
+
+
+])
